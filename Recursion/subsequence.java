ca40d9b365802ce568613e21fc34733017d7d6d8
tison132 <20euec072@skcet.ac.in>
2024-05-17 00:05:58 +0530

    sample file

--- Recursion/subsequence.java
+++ Recursion/subsequence.java
@@ -1,7 +1,18 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class subsequence {
     public static void main(String[] args) {
+        int arr[]={2,1,4};
+        List<List<Integer>> arrayList= new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        helper(0,arrayList,temp,arr);
+    }
+
+    private static void helper(int i, List<List<Integer>> arrayList,
+                               List<Integer> temp, int[] arr) {
 
     }
 }
